2adce3593c71c409899255200cce2249eae95bc9
EC2 Default User <ec2-user@ip-172-31-18-221.ap-northeast-1.compute.internal>
2021-07-09 00:18:09 +0000

    changeController

--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Goal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GoalController extends Controller
 {
@@ -14,18 +15,11 @@ class GoalController extends Controller
      */
     public function index()
     {
-        //
+        $goals = Auth::user()->goals;
+        
+        return response()->json($goals);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -35,29 +29,14 @@ class GoalController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Goal  $goal
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Goal $goal)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Goal  $goal
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Goal $goal)
-    {
-        //
+        $goal = new Goal();
+        $goal->title = request('title');
+        $goal->user_id = Auth::id();
+        $goal->save();
+        
+        $goals = Auth::user()->goals;
+        
+        return response()->json($goals);
     }
 
     /**
@@ -69,7 +48,13 @@ class GoalController extends Controller
      */
     public function update(Request $request, Goal $goal)
     {
-        //
+        $goal->title = request('title');
+        $goal->user_id = Auth::id();
+        $goal->save();
+        
+        $goals = Auth::user()->goals;
+        
+        return response()->json($goals);
     }
 
     /**
@@ -80,6 +65,10 @@ class GoalController extends Controller
      */
     public function destroy(Goal $goal)
     {
-        //
+        $goal->delete();
+        
+        $goals = Auth::user()->goals;
+        
+        return response()->json($goals);
     }
 }
